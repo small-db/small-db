6b270d62ad498b1f36e9d61c622b032468fedbb3
XiaochenCui <jcnlcxc.new@gmail.com>
2022-12-14 16:44:09 +0800

    update

--- tests/btree_delete_test.rs
+++ tests/btree_delete_test.rs
@@ -1,5 +1,5 @@
 mod test_utils;
-use log::error;
+use log::{debug, error};
 use small_db::{
     btree::{
         buffer_pool::BufferPool,
@@ -195,7 +195,7 @@ fn test_redistribute_internal_pages() {
 
 #[test]
 fn test_delete_internal_pages() {
-    let _ctx = test_utils::setup();
+    test_utils::setup();
 
     BufferPool::set_page_size(1024);
 
@@ -215,6 +215,8 @@ fn test_delete_internal_pages() {
     table.check_integrity(true);
 
     let root_pod = get_internal_page(&table, 0, 0);
+    let second_child_pod = get_internal_page(&table, 1, 1);
+
     assert_eq!(3, root_pod.rl().children_count());
 
     // Delete tuples causing leaf pages to merge until the first
@@ -222,55 +224,45 @@ fn test_delete_internal_pages() {
     let count =
         ceil_div(internal_children_cap(), 2) * leaf_records_cap();
     delete_tuples(&table, count);
+    assert_eq!(second_child_pod.rl().empty_slots_count(), 0);
 
-    // Deleting a page of tuples should bring the internal page below
+    // Deleting two pages of tuples should bring the internal page below
     // minimum occupancy and cause the entries to be redistributed.
-    table.draw_tree(2);
-    table.check_integrity(true);
-    let left_child_pod = get_internal_page(&table, 1, 0);
-    assert_eq!(
-        internal_children_cap() / 2,
-        left_child_pod.rl().empty_slots_count(),
-    );
-
-    let count = (ceil_div(internal_children_cap(), 2) - 1)
-        * leaf_records_cap();
+    let count = 2 * leaf_records_cap();
     delete_tuples(&table, count);
+    assert!(second_child_pod.rl().empty_slots_count() > 0);
 
-    table.draw_tree(2);
-    table.check_integrity(true);
-
-    // deleting another page of tuples should bring the page below
+    // Deleting another page of tuples should bring the page below
     // minimum occupancy again but this time cause it to merge
-    // with its right sibling
-    let count = leaf_records_cap();
+    // with its right sibling.
+    let count =
+        ceil_div(internal_children_cap(), 2) * leaf_records_cap();
     delete_tuples(&table, count);
 
-    // confirm that the pages have merged
-    let root_pod = get_internal_page(&table, 0, 0);
-    table.draw_tree(2);
-    table.check_integrity(true);
+    // Confirm that the pages have merged.
     assert_eq!(2, root_pod.rl().children_count());
 
     let e = BTreeInternalPageIterator::new(&root_pod.rl())
         .next()
         .unwrap();
-    let left_child_pod = get_internal_page(&table, 1, 0);
-    let right_child_pod = get_internal_page(&table, 1, 1);
-    assert_eq!(0, left_child_pod.rl().empty_slots_count());
+    let first_child_pod = get_internal_page(&table, 1, 0);
+    let second_child_pod = get_internal_page(&table, 1, 1);
+    table.draw_tree(2);
+    table.check_integrity(true);
     assert!(e.get_key().compare(
         Op::LessThanOrEq,
-        BTreeInternalPageIterator::new(&right_child_pod.rl())
+        BTreeInternalPageIterator::new(&second_child_pod.rl())
             .next()
             .unwrap()
             .get_key()
     ));
 
-    let count = internal_children_cap() * leaf_records_cap();
+    let count =
+        first_child_pod.rl().children_count() * leaf_records_cap();
     delete_tuples(&table, count);
 
-    // confirm that the last two internal pages have merged
-    // successfully and replaced the root
+    // Confirm that the last two internal pages have merged
+    // successfully and replaced the root.
     let root_pod = get_internal_page(&table, 0, 0);
     assert_eq!(0, root_pod.rl().empty_slots_count());
     table.draw_tree(2);
