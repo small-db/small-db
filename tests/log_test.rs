211af89299f59a49f95a9b319fd140dff3492f7e
XiaochenCui <jcnlcxc.new@gmail.com>
2023-02-06 19:47:14 +0800

    update version and publish cargo

--- tests/log_test.rs
+++ tests/log_test.rs
@@ -3,9 +3,14 @@ use small_db::{
     btree::page::BTreePage, transaction::Transaction,
     utils::HandyRwLock, BTreeTable, Tuple, Unique,
 };
-use test_utils::TreeLayout;
+use test_utils::{setup, TreeLayout};
 
 use crate::test_utils::{assert_true, look_for};
+
+fn insert_row(table: &BTreeTable, tx: &Transaction, key: i32) {
+    let tuple = Tuple::new_btree_tuple(key, 2);
+    table.insert_tuple(&tx, &tuple).unwrap();
+}
 
 /// Insert two tuples into the table, then commit the transaction.
 /// (There is a flush action in the middle of the transaction.)
@@ -15,15 +20,13 @@ fn commit_insert(table: &BTreeTable, key_1: i32, key_2: i32) {
     tx.start().unwrap();
 
     // step 2: insert a tuple into the table
-    let tuple = Tuple::new_btree_tuple(key_1, 2);
-    table.insert_tuple(&tx, &tuple).unwrap();
+    insert_row(&table, &tx, key_1);
 
     // step 3: force flush all pages (from the buffer pool to disk)
     Unique::buffer_pool().flush_all_pages();
 
     // step 4: insert another tuple into the table
-    let tuple = Tuple::new_btree_tuple(key_2, 2);
-    table.insert_tuple(&tx, &tuple).unwrap();
+    insert_row(&table, &tx, key_2);
 
     // step 5: commit the transaction
     tx.commit().unwrap();
@@ -37,10 +40,8 @@ fn abort_insert(table: &BTreeTable, key_1: i32, key_2: i32) {
     tx.start().unwrap();
 
     // step 2: insert two tuples into the table
-    let tuple_1 = Tuple::new_btree_tuple(key_1, 2);
-    table.insert_tuple(&tx, &tuple_1).unwrap();
-    let tuple_2 = Tuple::new_btree_tuple(key_2, 2);
-    table.insert_tuple(&tx, &tuple_2).unwrap();
+    insert_row(&table, &tx, key_1);
+    insert_row(&table, &tx, key_2);
 
     // step 3: search for the tuples
     assert_true(look_for(table, &tx, key_1) == 1, table);
@@ -107,79 +108,46 @@ fn test_abort() {
     assert_true(look_for(&table, &tx, 3) == 0, &table);
     assert_true(look_for(&table, &tx, 4) == 0, &table);
     tx.commit().unwrap();
+}
 
-    return;
+#[test]
+fn test_abort_commit_interleaved() {
+    setup();
 
+    let table_rc = test_utils::create_random_btree_table(
+        2,
+        0,
+        None,
+        1,
+        TreeLayout::Naturally,
+    );
+    let table = table_rc.rl();
+
+    commit_insert(&table, 1, 2);
+
+    // T1 start, T2 start and commit, T1 abort
+    let tx_1 = Transaction::new();
+    tx_1.start().unwrap();
+
+    // verify the result
     let tx = Transaction::new();
-    tx.start().unwrap();
-
-    let tuple = Tuple::new_btree_tuple(1, 2);
-    table.insert_tuple(&tx, &tuple).unwrap();
-    let tuple = Tuple::new_btree_tuple(2, 2);
-    table.insert_tuple(&tx, &tuple).unwrap();
-
-    tx.commit().unwrap();
-
-    let tx = Transaction::new();
-    tx.start().unwrap();
-
-    let tuple = Tuple::new_btree_tuple(4, 2);
-    table.insert_tuple(&tx, &tuple).unwrap();
-    let tuple = Tuple::new_btree_tuple(-1, 2);
-    table.insert_tuple(&tx, &tuple).unwrap();
-
-    assert!(test_utils::key_present(&tx, &table, 4));
-    assert!(test_utils::key_present(&tx, &table, -1));
-
-    tx.abort().unwrap();
-
     assert!(test_utils::key_present(&tx, &table, 1));
     assert!(test_utils::key_present(&tx, &table, 2));
-    assert!(!test_utils::key_present(&tx, &table, 3));
-    assert!(!test_utils::key_present(&tx, &table, 4));
-    assert!(!test_utils::key_present(&tx, &table, -1));
+    tx.commit().unwrap();
 
-    //     // insert tuples
-    //     void doInsert(HeapFile hf, int t1, int t2)
-    //     throws DbException, TransactionAbortedException,
-    // IOException {     Transaction t = new Transaction();
-    //     t.start();
-    //     if(t1 != -1)
-    //         insertRow(hf, t, t1, 0);
-    //     Database.getBufferPool().flushAllPages();
-    //     if(t2 != -1)
-    //         insertRow(hf, t, t2, 0);
-    //     t.commit();
-    // }
+    // Transaction t1 = new Transaction();
+    // t1.start();
+    // insertRow(hf1, t1, 3);
 
-    //    // insert tuples
-    // // force dirty pages to disk, defeating NO-STEAL
-    // // abort
-    // void dontInsert(HeapFile hf, int t1, int t2)
-    //     throws DbException, TransactionAbortedException,
-    // IOException {     Transaction t = new Transaction();
-    //     t.start();
-    //     if(t1 != -1)
-    //         insertRow(hf, t, t1, 0);
-    //     if(t2 != -1)
-    //         insertRow(hf, t, t2, 0);
-    //     if(t1 != -1)
-    //         look(hf, t, t1, true);
-    //     if(t2 != -1)
-    //         look(hf, t, t2, true);
-    //     abort(t);
-    // }
+    // Transaction t2 = new Transaction();
+    // t2.start();
+    // insertRow(hf2, t2, 21);
+    // Database.getLogFile().logCheckpoint();
+    // insertRow(hf2, t2, 22);
+    // t2.commit();
 
-    // @Test public void TestAbort()
-    // throws IOException, DbException, TransactionAbortedException {
-    // setup();
-    // doInsert(hf1, 1, 2);
-
-    // // *** Test:
-    // // insert, abort: data should not be there
-    // // flush pages directly to heap file to defeat NO-STEAL policy
-
-    // dontInsert(hf1, 4, -1);
+    // insertRow(hf1, t1, 4);
+    // abort(t1);
 
     // Transaction t = new Transaction();
     // t.start();
@@ -187,6 +155,7 @@ fn test_abort() {
     // look(hf1, t, 2, true);
     // look(hf1, t, 3, false);
     // look(hf1, t, 4, false);
+    // look(hf2, t, 21, true);
+    // look(hf2, t, 22, true);
     // t.commit();
-    // }
 }
