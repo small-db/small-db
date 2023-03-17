f8983922fc3f9e06dc453b5382f063e88d7467fb
XiaochenCui <jcnlcxc.new@gmail.com>
2023-03-17 12:35:00 +0800

    update

--- src/common/catalog.rs
+++ src/common/catalog.rs
@@ -49,62 +49,65 @@ impl Catalog {
             BTreeTable::new(catalog_file_path, 0, &catalog_schema);
 
         // scan the catalog table and load all the tables
-        let mut schemas = HashMap::new();
-        let mut table_names = HashMap::new();
+        // let mut schemas = HashMap::new();
+        // let mut table_names = HashMap::new();
 
         let tx = Transaction::new();
         tx.start()?;
         let mut iter = catalog_table.iter(&tx);
         let a = iter.next();
-        let b = iter.next();
-        while let Some(tuple) = iter.next() {
-            let table_id = tuple.get_cell(0).get_int64()?;
-            let table_name = tuple.get_cell(1).get_string()?;
-            let field_name = tuple.get_cell(2).get_string()?;
-            let field_type = tuple.get_cell(3).get_string()?;
-            let is_primary = tuple.get_cell(4).get_bool()?;
-
-            let mut fields = Vec::new();
-            fields.push(FieldItem::new(
-                &field_name,
-                Type::read_from(&mut SmallReader::new(
-                    field_type.as_bytes(),
-                )),
-                is_primary,
-            ));
-
-            schemas.insert(table_id, fields);
-            table_names.insert(table_id, table_name);
-        }
-
-        for (table_id, fields) in schemas {
-            let table_schema = Schema { fields };
-            let table_name = table_names.get(&table_id).unwrap();
-
-            let table_file_path = Path::new(catalog_file_path)
-                .parent()
-                .unwrap()
-                .join(table_name);
-            let table_file_path = table_file_path.to_str().unwrap();
-
-            let mut key_field = 0;
-            for (i, field) in table_schema.fields.iter().enumerate() {
-                if field.is_primary {
-                    key_field = i;
-                    break;
-                }
-            }
-
-            let table = BTreeTable::new(
-                table_file_path,
-                key_field,
-                &table_schema,
-            );
-            self.add_table(Arc::new(RwLock::new(table)));
-        }
 
         todo!()
     }
+
+    // while let Some(tuple) = iter.next() {
+    //     let table_id = tuple.get_cell(0).get_int64()?;
+    //     let table_name = tuple.get_cell(1).get_string()?;
+    //     let field_name = tuple.get_cell(2).get_string()?;
+    //     let field_type = tuple.get_cell(3).get_string()?;
+    //     let is_primary = tuple.get_cell(4).get_bool()?;
+
+    //     let mut fields = Vec::new();
+    //     fields.push(FieldItem::new(
+    //         &field_name,
+    //         Type::read_from(&mut SmallReader::new(
+    //             field_type.as_bytes(),
+    //         )),
+    //         is_primary,
+    //     ));
+
+    //     schemas.insert(table_id, fields);
+    //     table_names.insert(table_id, table_name);
+    // }
+
+    // for (table_id, fields) in schemas {
+    //     let table_schema = Schema { fields };
+    //     let table_name = table_names.get(&table_id).unwrap();
+
+    //     let table_file_path = Path::new(catalog_file_path)
+    //         .parent()
+    //         .unwrap()
+    //         .join(table_name);
+    //     let table_file_path = table_file_path.to_str().unwrap();
+
+    //     let mut key_field = 0;
+    //     for (i, field) in table_schema.fields.iter().enumerate() {
+    //         if field.is_primary {
+    //             key_field = i;
+    //             break;
+    //         }
+    //     }
+
+    //     let table = BTreeTable::new(
+    //         table_file_path,
+    //         key_field,
+    //         &table_schema,
+    //     );
+    //     self.add_table(Arc::new(RwLock::new(table)));
+    // }
+
+    // todo!()
+    // }
 
     pub fn get_table(&self, table_index: &Key) -> Option<&Value> {
         self.map.get(table_index)
