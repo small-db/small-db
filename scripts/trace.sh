a7f7f4e34582e3974a8fba2d4098fc36b457e7ef
XiaochenCui <jcnlcxc.new@gmail.com>
2023-06-01 18:47:26 +0800

    update

--- scripts/trace.sh
+++ scripts/trace.sh
@@ -8,9 +8,8 @@
 #   ./scripts/trace.sh <test_name>
 #
 # e.g:
-#   ./scripts/trace.sh integretions::btree_test::test_big_table
+#   ./scripts/trace.sh test_big_table
 
-# set -o xtrace
 set -o errexit
 set -o nounset
 
@@ -29,16 +28,16 @@ echo "BINARY: $BINARY"
 # run target, gather syscall
 # ===============================================================
 
-# clear logs
-sudo rm -f out
+# # clear logs
+# sudo rm -f out
 
-RUST_LOG=info \
-    sudo perf stat -e 'syscalls:sys_enter_*' -- \
-    $BINARY -- \
-    $TEST_NAME --exact --nocapture \
-    2>&1 | grep syscalls | sort \
-    >> out \
-    && sudo rm -rf data
+# RUST_LOG=info \
+#     sudo perf stat -e 'syscalls:sys_enter_*' -- \
+#     $BINARY -- \
+#     $TEST_NAME --exact --nocapture \
+#     2>&1 | grep syscalls | sort \
+#     >> out \
+#     && sudo rm -rf data
 
 # ===============================================================
 # run target, gather flamegraph
@@ -46,14 +45,15 @@ RUST_LOG=info \
 
 RUST_LOG=debug \
     # perf record -F 1000 --call-graph dwarf -- \
-    perf record -F 99 -g -- \
+    # sudo perf record -F 200 -g -- \
+    sudo perf record -F 99 -g -- \
     $BINARY -- \
     $TEST_NAME --exact --nocapture \
     >> out \
     && sudo rm -rf data
 
-perf script | ../FlameGraph/stackcollapse-perf.pl >out.perf-folded
+sudo perf script | ../FlameGraph/stackcollapse-perf.pl > out.perf-folded
 
-../FlameGraph/flamegraph.pl out.perf-folded >perf.svg
+../FlameGraph/flamegraph.pl out.perf-folded > perf.svg
 
 echo "Done, flamegraph: http://10.10.29.13:8000/perf.svg"
