e758c60adc29c4c130d33a11db7cd30ffd38a126
XiaochenCui <jcnlcxc.new@gmail.com>
2023-09-23 17:45:56 +0800

    update

--- tests/integretions/btree_test.rs
+++ tests/integretions/btree_test.rs
@@ -81,7 +81,7 @@ fn test_concurrent() {
 
     thread::scope(|s| {
         let mut insert_threads = vec![];
-        for i in 0..1000 {
+        for i in 0..10 {
         // for i in 0..20 {
             // thread local copies
             let local_table = table_pod.clone();
