e02379f07ba495970417d6691dc2ad7a305f90d9
Xiaochen Cui <jcnlcxc.new@gmail.com>
2024-05-30 16:49:59 -0700

    support pg command "\l"

--- src/sql/session/mod.rs
+++ src/sql/session/mod.rs
@@ -1,6 +1,7 @@
 use super::executor::{sql_handler::handle_sql, stream::Batch};
 use crate::{
     error::SmallError,
+    io::Encodeable,
     storage::{table_schema::Field, tuple::Tuple},
     transaction::Transaction,
 };
@@ -9,11 +10,15 @@ pub struct Session {}
 
 pub struct QueryResult {
     pub data: Vec<Tuple>,
+    cursor: usize,
 }
 
 impl QueryResult {
     pub fn new() -> Self {
-        Self { data: Vec::new() }
+        Self {
+            data: Vec::new(),
+            cursor: 0,
+        }
     }
 
     pub fn push_batch(&mut self, batch: &Batch) {
@@ -28,7 +33,20 @@ impl futures_core::stream::Stream for QueryResult {
         self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Option<Self::Item>> {
-        todo!()
+        if self.cursor >= self.data.len() {
+            return std::task::Poll::Ready(None);
+        }
+
+        let tuple = &self.data[self.cursor];
+        let mut bytes_list: Vec<Option<bytes::Bytes>> = Vec::new();
+        for cell in tuple.get_cells() {
+            let bytes = cell.to_bytes();
+            let v = bytes::Bytes::copy_from_slice(&bytes);
+            bytes_list.push(Some(v));
+        }
+
+        let data_row = pgwire::messages::data::DataRow::new(bytes_list);
+        return std::task::Poll::Ready(Some(Ok(data_row)));
     }
 }
 
