67de9097af9622c94e584990e2c5c37ca41f462e
Xiaochen Cui <jcnlcxc.new@gmail.com>
2024-06-01 15:41:06 -0700

    update vscode settings

--- tests/integretions/tx_isolation_test.rs
+++ tests/integretions/tx_isolation_test.rs
@@ -1,3 +1,4 @@
+use log::{debug, info};
 use small_db::{storage::tuple::Cell, transaction::Transaction, utils::HandyRwLock};
 
 use crate::test_utils::{insert_row, new_random_btree_table, search_key, setup, TreeLayout};
@@ -5,6 +6,7 @@ use crate::test_utils::{insert_row, new_random_btree_table, search_key, setup, T
 #[test]
 fn test_read_committed() {
     setup();
+
     let table_pod = new_random_btree_table(2, 0, None, 0, TreeLayout::LastTwoEvenlyDistributed);
 
     // start a read transaction
@@ -26,6 +28,12 @@ fn test_read_committed() {
     // commit, the read transaction should see it
     {
         write_tx.commit().unwrap();
+
+        info!("write_tx committed");
+        debug!(
+            "serach result: {}",
+            search_key(&table_pod.rl(), &read_tx, &Cell::Int64(key))
+        );
 
         assert!(search_key(&table_pod.rl(), &read_tx, &Cell::Int64(key)) == 1);
     }
