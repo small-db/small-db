456afbc432adc01a8d03ae67a1476253c04e071e
Xiaochen Cui <jcnlcxc.new@gmail.com>
2024-06-08 06:28:17 -0700

    rename "test_patch" to "test_flush_page"

--- tests/integretions/log_test.rs
+++ tests/integretions/log_test.rs
@@ -62,7 +62,7 @@ fn abort_insert(table: &BTreeTable, key_1: i64, key_2: i64) {
 #[test]
 #[cfg(feature = "aries_steal")]
 /// Test if the "flush_page" api writes "UPDATE" record to the log.
-fn test_patch() {
+fn test_flush_page() {
     use crate::test_utils::get_leaf_page;
 
     setup();
