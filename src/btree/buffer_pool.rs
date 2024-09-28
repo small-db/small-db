eb17110597bf8e0ea02c4e0c36a9d589a40bcf11
XiaochenCui <jcnlcxc.new@gmail.com>
2024-09-28 16:12:34 -0700

    update

--- src/btree/buffer_pool.rs
+++ src/btree/buffer_pool.rs
@@ -8,8 +8,6 @@ use std::{
     },
 };
 
-use log::info;
-
 use super::page::{
     BTreeHeaderPage, BTreeInternalPage, BTreeLeafPage, BTreePage, BTreePageID,
     BTreeRootPointerPage, PageCategory,
--- tests/integretions/benchmark.rs
+++ tests/integretions/benchmark.rs
@@ -1,12 +1,9 @@
 use std::{env, thread};
 
 use log::info;
-use rand::Rng;
-use small_db::{
-    btree::buffer_pool::BufferPool, transaction::Transaction, utils::HandyRwLock, Database,
-};
+use small_db::utils::HandyRwLock;
 
-use crate::test_utils::{insert_random, new_int_tuples, new_random_btree_table, setup, TreeLayout};
+use crate::test_utils::{insert_random, new_random_btree_table, setup, TreeLayout};
 
 #[test]
 #[cfg(feature = "benchmark")]
@@ -62,30 +59,4 @@ fn test_insert_parallel() {
         total_rows,
     );
     assert!(table.tuples_count() == total_rows);
-}
-
-#[test]
-#[cfg(feature = "benchmark")]
-fn test_insert_benchmark() {
-    // Use a small page size to speed up the test.
-    BufferPool::set_page_size(1024);
-
-    setup();
-
-    let row_count = 0;
-    let table_rc = new_random_btree_table(2, row_count, None, 0, TreeLayout::EvenlyDistributed);
-    let table = table_rc.rl();
-
-    // run insert, find the performance bottleneck
-    let mut rng = rand::thread_rng();
-
-    for _ in 0..3000 {
-        let tx = Transaction::new();
-
-        let insert_value = rng.gen_range(0, i64::MAX);
-        let tuple = new_int_tuples(insert_value, 2, &tx);
-        table.insert_tuple(&tx, &tuple).unwrap();
-
-        tx.commit().unwrap();
-    }
 }
