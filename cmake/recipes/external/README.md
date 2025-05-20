5d7a530643a3ec4972eecafe4cc3c84e96766c49
XiaochenCui <jcnlcxc.new@gmail.com>
2025-05-19 18:39:14 -0700

    update

---
+++ cmake/recipes/external/README.md
@@ -0,0 +1,3 @@
+# Summary
+
+This directory contains CMake recipes for external dependencies. The structure is similar to [cmake config from adobe/lagrange](https://github.com/adobe/lagrange/tree/8dde25b7ac7832445da3ccde5be8ecd645bc11a7/cmake/recipes/external).
--- play/run.sh
+++ play/run.sh
@@ -8,10 +8,12 @@ PLAY_DIR="/home/xiaochen/code/small-db/play"
 
 cmake \
     -S "$PLAY_DIR"/grpc-system \
-    -B "$PLAY_DIR"/gprc-system/build \
+    -B "$PLAY_DIR"/grpc-system/build \
     -G Ninja \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -DCMAKE_C_COMPILER=/usr/bin/clang-18 \
     -DCMAKE_CXX_COMPILER=/usr/bin/clang++-18 \
     -DCMAKE_PREFIX_PATH=~/local/cpplib
+
+cmake --build "$PLAY_DIR"/grpc-system/build
