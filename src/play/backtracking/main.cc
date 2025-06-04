d1bd6a8af3d1ddaca588db9533187055f1e26383
XiaochenCui <jcnlcxc.new@gmail.com>
2025-06-03 20:36:07 -0700

    update

--- src/play/backtracking/main.cc
+++ src/play/backtracking/main.cc
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <stdexcept>
 
-int main(int argc, char *argv[]) {
+#include <boost/stacktrace.hpp>
+
+void foo() {
+    throw std::runtime_error("An error occurred in foo");
+    std::cout << "foo" << std::endl;
+}
+
+void bar() {
+    foo();
+    std::cout << "bar" << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    try {
+        bar();
+    } catch (const std::exception& e) {
+        std::cout << "Caught exception: " << e.what() << std::endl;
+        std::cout << "Stacktrace:\n"
+                  << boost::stacktrace::stacktrace() << std::endl;
+    }
     std::cout << "backtracking play" << std::endl;
+    return 0;
 }
