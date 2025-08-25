ee38a95478f644708c0ec7f6e56ab7287fbffa4f
XiaochenCui <jcnlcxc.new@gmail.com>
2025-08-24 18:01:36 -0700

    stash changes

---
+++ jepsen/small-db-jepsen/test/small_db_jepsen/core_test.clj
@@ -0,0 +1,7 @@
+(ns small-db-jepsen.core-test
+  (:require [clojure.test :refer :all]
+            [small-db-jepsen.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
