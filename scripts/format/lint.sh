33eb34ae5d760200d787805516d38ae692875fbf
XiaochenCui <jcnlcxc.new@gmail.com>
2025-09-02 12:15:48 -0700

    stash changes

--- scripts/format/lint.sh
+++ scripts/format/lint.sh
@@ -4,12 +4,7 @@ set -o xtrace
 set -o errexit
 set -o nounset
 
-# install uv
-# curl -LsSf https://astral.sh/uv/install.sh | sh
-# uv tool install cpplint
 pip install cpplint
 pip install "cxc-toolkit>=0.8.2"
 
-# run cpplint
-# export PYTHONPATH=.
-python ./scripts/format/run-cpplint.py
+PYTHONPATH=. python ./scripts/format/run-cpplint.py
