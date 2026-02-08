e344010a962509fe449945318f84ccf0b3b6382b
XiaochenCui <jcnlcxc.new@gmail.com>
2026-02-08 07:45:58 -0800

    stash changes

--- scripts/format/lint.sh
+++ scripts/format/lint.sh
@@ -4,7 +4,4 @@ set -o xtrace
 set -o errexit
 set -o nounset
 
-pip install cpplint
-pip install "cxc-toolkit>=0.8.2"
-
-PYTHONPATH=. python ./scripts/format/run-cpplint.py
+PYTHONPATH=. uv run ./scripts/format/run-cpplint.py
