a7bea92cdc91053ea096338972fe38836f471d02
XiaochenCui <jcnlcxc.new@gmail.com>
2026-02-08 07:42:37 -0800

    stash changes

--- scripts/format/lint.sh
+++ scripts/format/lint.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "cpplint>=2.0.2",
+#     "cxc-toolkit>=1.1.2",
+# ]
+# ///
 
 set -o xtrace
 set -o errexit
